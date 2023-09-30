fde82ab3cf6fa6e3ee001ff61403c70dcb641392
user <9081603360ken@gmail.com>
2023-09-30 23:46:28 +0300

    4.0

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,42 +1,49 @@
 fun main() {
+    val comment = Comment(false, 1, 1, text = "1")
+
+    NoteService.createComment(comment)
+    NoteService.restoreComment(1, 1)
+
+    val list = NoteService.commentList
+    val result = list.last().statusComment
+    println(NoteService.commentList)
 
 }
 
 data class Notes(
-    val id: Int = 0,
-    val title: String = "Названи",
-    var text: String = "текст заметки"
+    val id: Int = 0, val title: String = "Названи", var text: String = "текст заметки"
 )   /* Заметки */
 
 data class Comment(
-    var statusComment: Boolean = true,
-    var idNote: Int,
-    var idComment: Int,
-    var text: String = "null"
+    var statusComment: Boolean = true, var idNote: Int, var idComment: Int, var text: String = "null"
 ) /* Комментарии */
 
 object NoteService {
 
-    private val noteList = mutableListOf<Notes>()
-    private val commentList = mutableListOf<Comment>()
+
+    val noteList = mutableListOf<Notes>()
+    val commentList = mutableListOf<Comment>()
+
 
     fun add(note: Notes): Notes {
         noteList += note.copy()
         return noteList.last()
     }                               /* Создает новую заметку у текущего пользователfun */
 
-    fun createComment(comment: Comment) {
+    fun createComment(comment: Comment): Comment {
         commentList += comment.copy()
+        return commentList.last()
 
     }              /* Добавляет новый комментарий к заметкee */
 
-    fun delete(id: Int) {
+    fun delete(id: Int): Boolean {
         for (i: Notes in noteList) {
             if (id == i.id) {
                 noteList.remove(i)
-                break
+                return true
             }
         }
+        return false
     }          /* Удаляет заметку текущего пользовател */
 
     fun deleteComment(noteId: Int, commentId: Int) {
@@ -47,21 +54,22 @@ object NoteService {
         }
     }              /* Удаляет комментарий к заметке */
 
-    fun edit(id: Int, text: String): Notes {
+    fun edit(id: Int, text: String): String {
         for (i: Notes in noteList) {
             if (id == i.id) {
                 i.text = text
-                break
             }
         }
-        return noteList.last()
+        return noteList.last().text
     }                              /* Редактирует заметку текущего пользовател */
 
-    fun editComment(idNote: Int, idComment: Int, text: String): Comment {
+    fun editComment(idNote: Int, idComment: Int, text: String): String {
         for (i in commentList) {
-            if (i.idNote == idNote && i.idComment == idComment) i.text = text
+            if (i.idNote == idNote && i.idComment == idComment) {
+                i.text = text
+            }
         }
-        return commentList.last()
+        return commentList.last().text
     }                 /* Редактирует указанный комментарий у заметк */
 
     fun get(): MutableList<Notes> {
@@ -69,13 +77,13 @@ object NoteService {
     }                                                   /* Возвращает список заметок, созданных пользователе */
 
 
-    fun getById(note: Notes): Any {
-
-        val index: Int = noteList.indexOf(note)
-        if (index != -1) {
-            return noteList[index]
+    fun getById(id: Int): Any {
+        for (i: Notes in noteList) {
+            if (id == i.id) {
+                return i
+            }
         }
-        return println("Заметка не найдена")
+        return false
     }                                                   /* Возвращает заметку по её id */
 
 
@@ -90,12 +98,18 @@ object NoteService {
         return commentToNote
     } /* Возвращает список комментариев к заметке */
 
-    fun restoreComment(commentId: Int, noteId: Int) {
+    fun restoreComment(commentId: Int, noteId: Int): Boolean {
         for (i in commentList) {
             if (i.idNote == noteId && i.idComment == commentId) {
                 i.statusComment = true
+                return i.statusComment
             }
         }
+        return false
+    }/* Восстанавливает удалённый комментарий. */
+
+    fun clear() {
+        noteList.clear()
+        commentList.clear()
     }
-    /* Восстанавливает удалённый комментарий. */
 }
--- src/test/kotlin/NoteServiceTest.kt
+++ src/test/kotlin/NoteServiceTest.kt
@@ -1,12 +1,201 @@
+import junit.framework.TestCase.*
+import org.junit.Before
 import org.junit.Test
 
-class NoteServiceTest{
+
+class NoteServiceTest {
+
+    @Before
+    fun clear() {
+        NoteService.clear()
+    }
+
     @Test
-    fun add(){
+    fun add() {
+        val note = Notes(1, "1", "1")
+        val result = NoteService.add(note)
+        assertEquals(result, note)
+    }
+
+    @Test
+    fun createComment() {
+        val comment = Comment(true, 1, 1, text = "1")
+        val result = NoteService.createComment(comment)
+        assertEquals(result.text, comment.text)
 
     }
-    @Test
-    fun edi(){
 
+    @Test
+    fun deleteTrue() {
+        val notes = Notes(1, "1", "1")
+        val idNote = 1
+        NoteService.add(notes)
+        val result = NoteService.delete(idNote)
+        assertTrue(result)
+
+    }
+
+    @Test
+    fun deleteFalse() {
+        val notes = Notes(1, "1", "1")
+        val idNote = 2
+        NoteService.add(notes)
+        val result = NoteService.delete(idNote)
+        assertFalse(result)
+    }
+
+    @Test
+    fun deleteCommentFalse() {
+        val comment = Comment(true, 1, 1, text = "1")
+        NoteService.createComment(comment)
+        NoteService.deleteComment(1, 1)
+        val list = NoteService.commentList
+        val result = list.last().statusComment
+        assertFalse(result)
+    }
+
+    @Test
+    fun deleteCommentTrue() {
+        val comment = Comment(true, 1, 1, text = "1")
+        NoteService.createComment(comment)
+        NoteService.deleteComment(2, 1)
+        val list = NoteService.commentList
+        val result = list.last().statusComment
+        assertTrue(result)
+    }
+
+    @Test
+    fun deleteCommentTrue1() {
+        val comment = Comment(true, 1, 1, text = "1")
+        NoteService.createComment(comment)
+        NoteService.deleteComment(1, 2)
+        val list = NoteService.commentList
+        val result = list.last().statusComment
+        assertTrue(result)
+    }
+
+    @Test
+    fun edit() {
+        val text = "редактирована"
+        val notes = Notes(1, "1", "1")
+        NoteService.add(notes)
+        val result = NoteService.edit(1, text)
+        assertEquals(result, text)
+    }
+
+    @Test
+    fun edit2() {
+        val text = "редактирована"
+        val notes = Notes(1, "1", "1")
+        NoteService.add(notes)
+        val result = NoteService.edit(2, text)
+        assertEquals(result, "1")
+    }
+
+    @Test
+    fun editComment() {
+        val text = "редактирована"
+        val comment = Comment(true, 1, 1, text = "1")
+        NoteService.createComment(comment)
+        val result = NoteService.editComment(1, 1, text)
+        assertEquals(result, text)
+    }
+
+    @Test
+    fun editComment1() {
+        val text = "редактирована"
+        val comment = Comment(true, 1, 1, text = "1")
+        NoteService.createComment(comment)
+        val result = NoteService.editComment(2, 1, text)
+        assertEquals(result, "1")
+    }
+
+    @Test
+    fun editComment2() {
+        val text = "редактирована"
+        val comment = Comment(true, 1, 1, text = "1")
+        NoteService.createComment(comment)
+        val result = NoteService.editComment(1, 2, text)
+        assertEquals(result, "1")
+    }
+
+    @Test
+    fun get() {
+        val note = Notes(1, "1", "1")
+        val note1 = Notes(2, "2", "2")
+        NoteService.add(note)
+        NoteService.add(note1)
+        val result = NoteService.get()
+        assertEquals(result, mutableListOf(note, note1))
+    }
+
+    @Test
+    fun getByIdOk() {
+        val note = Notes(1, "1", "1")
+        NoteService.add(note)
+        val result = NoteService.getById(1)
+        assertEquals(result, note)
+    }
+
+    @Test
+    fun getByIdFalse() {
+        val note = Notes(1, "1", "1")
+        NoteService.add(note)
+        val result = NoteService.getById(2)
+        assertFalse(result as Boolean)
+    }
+
+    @Test
+    fun getComments() {
+        val comment = Comment(true, 1, 1, text = "1")
+        val comment1 = Comment(true, 1, 2, text = "2")
+        NoteService.createComment(comment)
+        NoteService.createComment(comment1)
+        val result = NoteService.getComments(1)
+        assertEquals(result, mutableListOf(comment, comment1))
+    }
+
+    @Test
+    fun getComments1() {
+        val comment = Comment(true, 1, 1, text = "1")
+        val comment1 = Comment(false, 1, 2, text = "2")
+        NoteService.createComment(comment)
+        NoteService.createComment(comment1)
+        val result = NoteService.getComments(1)
+        assertEquals(result, mutableListOf(comment))
+    }
+
+    @Test
+    fun getComments2() {
+        val comment = Comment(true, 1, 1, text = "1")
+        val comment1 = Comment(true, 2, 2, text = "2")
+        NoteService.createComment(comment)
+        NoteService.createComment(comment1)
+        val result = NoteService.getComments(1)
+        assertEquals(result, mutableListOf(comment))
+    }
+
+    @Test
+    fun restoreComment() {
+        val comment = Comment(false, 1, 1, text = "1")
+        NoteService.createComment(comment)
+        val result = NoteService.restoreComment(1, 1)
+        assertTrue(result)
+    }
+
+    @Test
+    fun restoreComment1() {
+        val comment = Comment(false, 1, 1, text = "1")
+        NoteService.createComment(comment)
+        val result = NoteService.restoreComment(2, 1)
+        assertFalse(result)
+    }
+
+    @Test
+    fun restoreComment2() {
+        val comment = Comment(false, 1, 1, text = "1")
+        NoteService.createComment(comment)
+        val result = NoteService.restoreComment(1, 2)
+        assertFalse(result)
     }
 }
